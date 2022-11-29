6e1fadf194406cf50b5311a32f6fee39cd6079bd
Hayden Chen <brooklyn1984@qq.com>
2022-11-29 16:44:56 +0800

    feat: ✨ upgrade logseq/libs react typescript antd vite

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App: React.FC<{ env: string }> = ({ env }) => {
   return (
     <div className="w-screen h-screen flex items-center justify-center text-white">
       <div className="w-screen h-screen fixed top-0 left-0" onClick={() => logseq.hideMainUI()}></div>
-      <div className="w-5/6 h-5/6 z-0 bg-gradient-to-tr from-yellow-400 via-red-500 to-pink-500 flex flex-col items-center justify-center">
+      <div className="w-5/6 h-5/6 z-0 bg-gradient-to-tr from-green-300 via-green-500 to-green-700 flex flex-col items-center justify-center">
         <h1 className="font-bold text-4xl">Logseq-plugin-react-boilerplate</h1>
         <h2 className="text-2xl mt-6">Current Env: {env}</h2>
       </div>
